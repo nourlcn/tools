7274cf59c6757bc652ab2e7272ef742d7b66d891
Nourl <nourlcn@gmail.com>
2011-11-12 21:14:59 +0800

    fix bugs in get_img.py

--- get_img.py
+++ get_img.py
@@ -1,36 +1,31 @@
 import hashlib
-import os,sys
+import os
 
-
-
-def get_img_url(id):
-    int_id = int(id);
+def get_img_url(appid):
+    int_id = int(appid);
     yu = int_id%5000;
-
     post_fix = str(yu);
-
-    img_url = "/srv/nfs/img/"+ post_fix + "/" + id[:-1] + "_m.png";
+    img_url = "/srv/nfs/img/"+ post_fix + "/" + appid[:-1] + "_m.png";
     return img_url; 
-    
 
 def CalcMD5(filepath):
     f = file(filepath,'rb')
     if f:
         md5obj = hashlib.md5()
         md5obj.update(f.read())
-        hash = md5obj.hexdigest()
-        return hash
+        hashv = md5obj.hexdigest()
+        return hashv
     else:
         return "000"
         
 if __name__ == "__main__":
     count = 0
-    id=[]
+    appid=[]
     img=[]
     md5=[]
     fin = file('./pvsz','r')
     for x in fin:
-        id.append(x)
+        appid.append(x)
         img.append(get_img_url(x))
     
     for x in img:
@@ -47,16 +42,11 @@ if __name__ == "__main__":
     
     fout = file('./id_md5','w')
     
-    for x in range(len(id)-1):
-        fout.write(id[x]+md5[x]+"\n")
+    for x in range(len(appid)-1):
+        fout.write(appid[x]+md5[x]+"\n")
     
     fout.close()
     fin.close()
     
-    print len(id) - count
+    print len(appid) - count
     print len(set(md5))
-
-
-
-
-
---
+++ multiprocess.py
@@ -0,0 +1,18 @@
+from multiprocessing import Pool, Process
+import os
+
+def info(title):
+    print title
+    print 'module name',__name__
+    print 'parent process',os.getppid()
+    print 'current process',os.getpid()
+
+def f(name):
+    info("function f")
+    print "hello",name
+
+if __name__ == '__main__':
+    info('main line')
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
